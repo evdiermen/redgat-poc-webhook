e74e67f3c1cb801d75492c588fa0dad3c2667c95
unknown <jdeuling@WEHKAMP>
2016-12-23 13:24:33 +0100

    test jelmer

--- hr/HR/Tables/HR_EVD_TEST2.sql
+++ hr/HR/Tables/HR_EVD_TEST2.sql
@@ -2,5 +2,6 @@ CREATE TABLE hr.hr_evd_test2 (
   abc VARCHAR2(10 BYTE),
   def VARCHAR2(10 BYTE),
   getal NUMBER(10,2),
-  ghi VARCHAR2(10 BYTE)
+  ghi VARCHAR2(10 BYTE),
+  jelmer VARCHAR2(30 BYTE)
 );
